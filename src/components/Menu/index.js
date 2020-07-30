9a26ea304ed2ee7c4b50dd94fff45e434463ef60
Peu <peu@coffeecup.cc>
2020-07-30 10:50:23 -0300

    Pagina de categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+import {Link} from 'react-router-dom'; 
 
 // import ButtonLink from '../ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Cornoflix logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Nova Dor
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/Video';
+import Home from './pages/Home';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/cadastro/video" component={CadastroVideo} exact/>
+    <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+    <Route path="/" component={Home} exact/>
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
